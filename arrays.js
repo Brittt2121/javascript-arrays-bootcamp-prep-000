31f03761e7ab3ad59e7b67f299396212037411ef
Brittany Bellantoni <brittt2121@hotmail.com>
2017-11-25 21:21:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat"];
 
-var addElementToBeginningOfArray = ["element1", "element2"];
-addElementToBeginningOfArray.push("element0");
+function addElementToBeginningOfArray()
